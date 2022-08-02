e65965fbba6cdbd6ac54c351d59ae4b58790045f
Yoni Schirris <yschirris@gmail.com>
2022-08-02 18:31:32 +0200

    add notebook component

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 import os.path as ops
 import lightning as L
 from quick_start.components import PyTorchLightningScript, ImageServeGradio
+from lit_jupyter import LitJupyter
 
 class TrainDeploy(L.LightningFlow):
     def __init__(self):
         super().__init__()
+        self.lit_jupyter = LitJupyter()
         self.train_work = PyTorchLightningScript(
             script_path=ops.join(ops.dirname(__file__), "./train_script.py"),
             script_args=["--trainer.max_epochs=5"],
@@ -15,6 +17,7 @@ class TrainDeploy(L.LightningFlow):
     def run(self):
         # 1. Run the python script that trains the model
         self.train_work.run()
+        self.lit_jupyter.run()
 
         # 2. when a checkpoint is available, deploy
         if self.train_work.best_model_path:
@@ -23,6 +26,7 @@ class TrainDeploy(L.LightningFlow):
     def configure_layout(self):
         tab_1 = {"name": "Model training", "content": self.train_work}
         tab_2 = {"name": "Interactive demo", "content": self.serve_work}
-        return [tab_1, tab_2]
+        tab_3 = {"name": "Notebook", "content": self.lit_jupyter}
+        return [tab_1, tab_2, tab_3]
 
 app = L.LightningApp(TrainDeploy())
